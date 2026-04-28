32a8d9da050611d3b5d0784fe47ef81b774b96ba
Usuario <geral.gonzalezs@duocuc.cl>
2026-04-27 22:20:10 -0400

    Estructura inicial del proyecto carritos

---
+++ src/main/java/com/proyecto/carritos_proyecto/CarritosProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.carritos_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritosProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritosProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/carritos_proyecto/CarritosProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.carritos_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritosProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
