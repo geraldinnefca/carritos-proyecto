3104b47fba872555d93122c81c53ad62c96c91d0
Usuario <geral.gonzalezs@duocuc.cl>
2026-05-06 21:57:09 -0400

    Desarrollo CarritoRequestDTO y CarritoResponseDTO

---
+++ src/main/java/cl/sda1085/carritos/dto/CarritoRequestDTO.java
@@ -0,0 +1,27 @@
+package cl.sda1085.carritos.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+
+public class CarritoRequestDTO {
+
+        @NotNull(message = "ID de usuario es obligatorio")
+        private Long idUsuario;
+
+        @NotNull(message = "ID de producto es obligatorio")
+        private Long idProducto;
+
+        @Min(value = 1, message = "La cantidad mínima es 1")
+        private Integer cantidad;
+
+        @NotNull(message = "El precio es obligatorio")
+        @Positive(message = "El precio debe ser mayor a cero")
+        private BigDecimal precioUnitario;
+    }
+
---
+++ src/main/java/cl/sda1085/carritos/dto/CarritoResponseDTO.java
@@ -0,0 +1,23 @@
+package cl.sda1085.carritos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+
+public class CarritoResponseDTO {
+
+    private Long id;
+    private Long idUsuario;
+    private Long idProducto;
+    private Integer cantidad;
+    private BigDecimal precioUnitario;
+    private BigDecimal subtotal; // Campo calculado
+    private String estado;
+    private LocalDateTime fechaAgregado;
+
+}
